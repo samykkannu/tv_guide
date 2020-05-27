20fba1e05f2a098b27cef38495b50d31240a0ae7
samykkannu <samykkannu@knowde.com>
2020-05-27 12:37:45 +0530

    initial

--- app/models/user_faverite.rb
+++ app/models/user_faverite.rb
@@ -4,7 +4,7 @@ class UserFaverite < ApplicationRecord
   after_save :notify_user
 
   def notify_user
-    time = Show.find(show_id).timings - 30.minuts
+    time = Show.find(show_id).timings - 30.minutes
     ShowNotifierWorker.perform_at(time, show_id)
   end
 end
